e75e9a9c00743e8eae3c0675718192776331857b
David-Melamed <davidmelamed269@gmail.com>
2024-06-21 13:14:47 +0300

    replace db name hypens with underscore to meet format requierments

--- modules/rds/instance.tf
+++ modules/rds/instance.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "mysql_db" {
   instance_class = var.instance_class
   username = var.username
   password = var.password
-  db_name = var.db_name
+  db_name = replace(var.db_name, "-", "_")
   skip_final_snapshot = var.skip_final_snapshot
   vpc_security_group_ids = var.vpc_security_group_id
   db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name

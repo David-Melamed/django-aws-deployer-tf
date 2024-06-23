587ff2e1d7340308d1420bba90b24a1012aadd24
David-Melamed <davidmelamed269@gmail.com>
2024-06-23 18:57:32 +0300

    add repo name as output

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -2,6 +2,6 @@ output "ecr_repository_url" {
     value = aws_ecrpublic_repository.django_app.repository_uri
 }
 
-output "ecr_readiness" {
-    value = null_resource.push_image_to_ecr
+output "ecr_repository_name" {
+    value = aws_ecrpublic_repository.django_app.repository_name
 }

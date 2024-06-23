22ee1a6943ec13532e4084b7d6ad22d3a3b4d5bb
David-Melamed <davidmelamed269@gmail.com>
2024-06-23 19:25:42 +0300

    add to s3 bucket name random string and convert to lowercases

--- modules/s3/env_bucket.tf
+++ modules/s3/env_bucket.tf
@@ -1,5 +1,13 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+locals {
+  bucket_name = lower(var.bucket_name)
+}
+
 resource "aws_s3_bucket" "beanstalk_bucket" {
-  bucket = lower(var.bucket_name)
+  bucket = "${local.bucket_name}-${random_id.bucket_suffix.hex}"
   object_lock_enabled = false
 
   tags = {

8b88a5c2072f1c6ddb81207bd6075b7f44882ddb
David-Melamed <davidmelamed269@gmail.com>
2024-06-23 19:00:24 +0300

    added asterik to get all image details

--- modules/ecr/scripts/cleanup_ecr_repo.sh
+++ modules/ecr/scripts/cleanup_ecr_repo.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
-
 REPO_NAME=$1
 REGION=$2
 
-if [ -z "$REPO_NAME" ] || [ -z "$REGION" ]; then
-    echo "Usage: $0 <repository-name> <region>"
+check_exit_status() {
+  if [ $? -ne 0 ]; then
+    echo "Error: $1"
     exit 1
-fi
-
-echo "Starting cleanup of ECR repository: $REPO_NAME in region: $REGION"
+  fi
+}
 
 # Get the list of image digests
-image_digests=$(aws ecr-public describe-images --repository-name "$REPO_NAME" --region "$REGION" --query 'imageDetails[].imageDigest' --output text)
+image_digests=$(aws ecr-public describe-images --repository-name "$REPO_NAME" --region "$REGION" --query 'imageDetails[*].imageDigest' --output text)
+check_exit_status "Failed to retrieve image digests."
 
-if [ -z "$image_digests" ]; then
-    echo "No images found in the repository $REPO_NAME"
-    exit 0
-fi
 
-# Delete images by digest
+# Loop through the list and delete each image
 for digest in $image_digests; do
-    aws ecr-public batch-delete-image --repository-name "$REPO_NAME" --region "$REGION" --image-ids imageDigest="$digest"
-    echo "Deleted image with digest: $digest"
+  aws ecr-public batch-delete-image --repository-name "$REPO_NAME" --region "$REGION" --image-ids imageDigest="$digest"
 done
+check_exit_status "Failed to delete image with digest: $digest"
 
-echo "All images have been deleted from the repository $REPO_NAME"

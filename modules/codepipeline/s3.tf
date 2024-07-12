1b63214c05a02e71de20443d759b137356f08d66
David-Melamed <davidmelamed269@gmail.com>
2024-07-12 19:20:10 +0300

    add trigger to upload script when the file is changed

--- modules/codepipeline/s3.tf
+++ modules/codepipeline/s3.tf
@@ -4,6 +4,7 @@ resource "aws_s3_object" "dockerfile" {
   source = "${path.module}/scripts/Dockerfile"
   acl    = "public-read"
   depends_on = [ var.bucket_policy_arn ]
+  etag = filesha256("${path.module}/scripts/Dockerfile")
 }
 
 resource "aws_s3_object" "override_settings" {
@@ -12,6 +13,7 @@ resource "aws_s3_object" "override_settings" {
   source = "${path.module}/scripts/override_settings.py"
   acl    = "public-read"
   depends_on = [ var.bucket_policy_arn ]
+  etag = filesha256("${path.module}/scripts/override_settings.py")
 }
 
 resource "aws_s3_object" "custom_settings" {
@@ -20,4 +22,5 @@ resource "aws_s3_object" "custom_settings" {
   source = "${path.module}/scripts/custom_settings.py"
   acl    = "public-read"
   depends_on = [ var.bucket_policy_arn ]
+  etag = filesha256("${path.module}/scripts/custom_settings.py")
 }

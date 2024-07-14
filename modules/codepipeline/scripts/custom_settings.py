9146d5eeec94d0a254151127d054b1b42e41faff
David-Melamed <davidmelamed269@gmail.com>
2024-07-14 20:45:44 +0300

    update except block

--- modules/codepipeline/scripts/custom_settings.py
+++ modules/codepipeline/scripts/custom_settings.py
@@ -1,6 +1,7 @@
 from .settings import *
 
+# Override these settings with local settings if such a file exists
 try:
     from .override_settings import *
-except ImportError:
+except ImportError as e:
     pass

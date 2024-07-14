2dfde4e23cecd1181c907ea20d076cac9eb21a03
David-Melamed <davidmelamed269@gmail.com>
2024-07-14 20:57:05 +0300

    add validation on app name and remove default values to set values as inputs

--- variables.tf
+++ variables.tf
@@ -61,7 +61,7 @@ variable "engine" {
 variable "engine_version" {
   description = "Version of the MySQL database engine"
   type        = string
-  default     = "5.7"
+  default     = "8.0.35"
 }
 
 variable "skip_final_snapshot" {
@@ -116,13 +116,16 @@ variable "db_password" {
 variable "app_name" {
   description = "Applcation name"
   type        = string
-  default     = "swapapp"
+ 
+  validation {
+    condition = can(regex("^[a-z0-9_-]+$", var.app_name))
+    error_message = "The application name can only contain lowercase letters, numbers, underscores, and hyphens."
+  }
 }
 
 variable "django_project_url" {
   description = "Django project URL"
   type        = string
-  default = "https://github.com/David-Melamed/django-swap-app.git"
 }
 
 variable "branch_name" {

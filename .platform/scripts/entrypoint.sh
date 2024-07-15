f2531e6390e06a9aa1bdc153b9fa3bfecab27d11
David-Melamed <davidmelamed269@gmail.com>
2024-07-15 21:29:24 +0300

    add collectstatic command

--- .platform/scripts/entrypoint.sh
+++ .platform/scripts/entrypoint.sh
@@ -45,6 +45,7 @@ fi
 django_commands=(
     "python manage.py makemigrations"
     "python manage.py migrate"
+    "python manage.py collectstatic --noinput"
     "python manage.py runserver 0.0.0.0:9090"
 )
 

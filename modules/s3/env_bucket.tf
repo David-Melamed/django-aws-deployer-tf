0d47a666cccc1398bcfa71ebde677140a65c7545
David-Melamed <davidmelamed269@gmail.com>
2024-07-28 15:09:11 +0300

    combine existing tags with generic tags

--- modules/s3/env_bucket.tf
+++ modules/s3/env_bucket.tf
@@ -2,17 +2,10 @@ resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-locals {
-  bucket_name = lower(var.bucket_name)
-}
-
 resource "aws_s3_bucket" "beanstalk_bucket" {
   bucket = "${local.bucket_name}-${random_id.bucket_suffix.hex}"
   object_lock_enabled = false
-
-  tags = {
-          "elasticbeanstalk:environment-name" = format("%s-%s", var.beanstalk_app_name, var.env)
-  }
+  tags = local.combined_tags
 }
 
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {

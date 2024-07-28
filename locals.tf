472b216ceb9b4ed81b4a2c00e27f64bb216db274
David-Melamed <davidmelamed269@gmail.com>
2024-07-28 15:08:01 +0300

    add generic_tags local including app name and env

--- locals.tf
+++ locals.tf
@@ -2,4 +2,8 @@ locals {
   db_host            = "rds-${var.env}.${var.zone_name}"
   repo_owner         = regex("https://github.com/([^/]+)/", var.django_project_url)[0]
   repo_name          = regex("https://github.com/[^/]+/([^/]+).git", var.django_project_url)[0]
+  generic_tags = {
+    ApplicationName = var.app_name,
+    Environment     = var.env
+  }
 }

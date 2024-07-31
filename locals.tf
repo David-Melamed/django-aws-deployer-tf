ec962ebd7418bec2bcbe4f358b367260cf820166
David-Melamed <davidmelamed269@gmail.com>
2024-07-31 11:41:46 +0300

    remove DateCreation and CreatorName to avoid resources duplication and rename application name and environment name tags for consistency

--- locals.tf
+++ locals.tf
@@ -2,17 +2,8 @@ locals {
   db_host            = "rds-${var.env}.${var.zone_name}"
   repo_owner         = regex("https://github.com/([^/]+)/", var.django_project_url)[0]
   repo_name          = regex("https://github.com/[^/]+/([^/]+).git", var.django_project_url)[0]
-  date_creation      = formatdate("YYYY-MM-DD", timestamp())
-  creator_name       = data.external.current_user.result.creator_name
-  
   generic_tags = {
-    ApplicationName = var.app_name,
-    Environment     = var.env,
-    DateCreation    = formatdate("YYYY-MM-DDTHH:MM:SSZ", timestamp())
-    CreatorName     = local.creator_name
+    application-name = var.app_name,
+    environment-name = var.env
   }
-}
-
-data "external" "current_user" {
-  program = ["sh", "-c", "echo '{\"creator_name\": \"'$(whoami)'\"}'"]
 }
